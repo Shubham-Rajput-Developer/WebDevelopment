2679eabfad9bb83bd9f10c22a15984dceec6fb09
Shubham Rajput <shubhamrajput0504@gmail.com>
2024-06-17 14:01:50 +0530

    Create part1.js

---
+++ JavaScript/part1.js
@@ -0,0 +1,202 @@
+// console.log("Shubham");
+// let a = 10;
+// let b = 20;
+// // console.log("Addition is : ", (a+b));
+// console.log(`Your total price is : ${a+b} rupee`);
+// let age = 2
+// if(age < 18){
+//     console.log("You can not vote");
+// }
+// else if(age > 18 ){
+//     console.log("You can vote");
+// }
+// else{
+//     console.log("NO Freedom");
+// }
+
+// //Traffic signle 
+// let color = "yellow";
+// if(color == "red"){
+//     console.log("Stop");
+// }
+// else if(color == "yellow"){
+//     console.log("Ready");
+// }
+// else if(color == "green"){
+//     console.log("Goooo");
+// }
+// else{
+//     console.log("Colapess");
+// }
+
+// //popcorn price
+
+// let size = "m";
+
+// if(size === "XL"){
+//     console.log("popcorn price is 250");
+// }
+// else if(size === "L"){
+//     console.log("popcorn price is 200");
+// }
+// else if(size === "M"){
+//     console.log("popcorn price is 100");
+// }
+// else if(size === "S"){
+//     console.log("popcorn price is 50");
+// }
+// else{
+//     console.log("Wrong size");
+// }
+
+
+// //Check greater no.
+
+// let m1 = 10;
+// let m2 = 3;
+// let m3 = 2;
+// if(m1>m2){
+//     if(m1>m3){
+//         console.log("Greater is m1");
+//     }
+//     else{
+//         console.log("Greater is m3");
+//     }
+// }
+// else{
+//     if(m2>m3){
+//         console.log("Greter is m2");
+//     }
+//     else
+//     {
+//         console.log("Greater is m3");
+//     }
+// }
+
+// //Practise QUE
+
+// let str = "app";
+// if((str[0]==='a') && (str.length >= 3)){
+//     console.log("Good String");
+// }
+// else{
+//     console.log("Not Good String");
+// }
+
+// //Switch case statement
+
+// let day = 5;
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thrusday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday"); 
+//         break;   
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Wrong input");    
+//         break;
+// }
+// // alert("Hello pajji Shubham");
+// // prompt("Enter your name : ");
+// // let pop = prompt("Enter your name ");
+// // console.log(pop);
+
+// let fname = prompt("Enter first name : ");
+// let lname = prompt("Enter last name : ");
+// let msg = "Welcome "+fname+" "+lname;
+// console.log(msg);
+// alert(msg);
+
+//Question 1
+// let num = 20;
+// if(num%10==0){
+//     console.log("Good");
+// }
+// else{
+//     console.log("Bad");
+// }
+
+//Question 2
+// let fname = prompt("Enter name : ");
+// let age = prompt("Enter age : ");
+// let msg =  `${fname} is ${age} years old.`;
+// alert(msg);  
+// console.log(msg);
+
+//Question 3
+// let quater = 3;
+// switch(quater){
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+//     case 2:
+//         console.log("April,May,June");
+//         break;
+//     case 3:
+//         console.log("July,August,September");
+//         break;
+//     case 4:
+//         console.log("October,November,December");
+//         break;
+//     default:
+//         console.log("Wrong input");
+// }
+
+//Question 4
+// let str = "Akshda ";
+// if((str[0]=='A'||str[1]=='a')&&(str.length>5)){
+//     console.log("Golden String");
+// }
+// else{
+//     console.log("Not golden String");
+// }
+
+//Question 5
+// let m1=90;
+// let m2=78;
+// let m3=200;
+// if(m1>m2){
+//     if(m1>m3){
+//         console.log(m1," is greater Number");
+//     }
+//     else
+//     {
+//         console.log(m3," is greater Number");
+//     }
+// }
+// else{
+//     if(m2>m3){
+//         console.log(m2," is greater Number");
+//     }
+//     else
+//     {
+//         console.log(m3," is greater Number")
+//     }
+// }
+
+//Question 6
+// let m1 = prompt("Enter Number 1 : ");
+// let m2 = prompt("Enter Number 2 : ");
+// if(m1[m1.length-1]==m2[m2.length-1]){    //also used (m1%10==m2%10)
+//     console.log("Last digit is Same");
+// }
+// else{
+//     console.log("Not same");
+// }
+
