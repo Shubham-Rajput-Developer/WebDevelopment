c1880aaa6e68ba89437641926171749a19a4345b
Shubham Rajput <shubhamrajput0504@gmail.com>
2024-06-20 17:45:01 +0530

    Update part5.js

--- JavaScript/part5.js
+++ JavaScript/part5.js
@@ -16,3 +16,90 @@
 // console.log(post);
 // console.log(post["username"]);
 // console.log(post.repost);
+
+// let datatype = {
+//     null : "Its key is null",
+//     let : "Its key is let ",
+//     true : "Its key is true"
+// }
+// console.log(datatype);
+
+//Operation on object literals
+// let student = {
+//     name : "Shubham",
+//     age : 23,
+//     marks : 94.4,
+//     city : "dehli"
+// }
+// console.log(student);
+// student.city="mumbai";
+// console.log(student);
+// student.gender =  "Male";
+// console.log(student);
+// student.age = 25;
+// console.log(student);
+// delete student.age;
+// console.log(student);
+
+// //Nested Object
+// const Class = {
+//     shubham : {
+//         grade : "A+",
+//         city : "jalgoan"
+//     },
+//     akshada : {
+//         grade : "B+",
+//         city : "pachora"
+//     },
+//     shubhangi : {
+//         grade : "A++",
+//         city : "nagar"
+//     }
+// };
+// console.log(Class);
+// console.log(Class.shubham);
+// console.log(Class.shubhangi.city);
+
+// //Array of object
+// let Class = [
+//     {
+//         name : "Shubham",
+//         city : "jalgoan"
+//     },
+//     {          
+//         name : "Akshada",
+//         city : "pachora"
+//     },
+//     {
+//         name : "Shubhangi",
+//         city : "nagar"
+//     }
+// ];
+// console.log(Class);
+// console.log(Class[1]);
+// console.log(Class[2].city);
+
+//Question 1
+let num = Math.floor(Math.random()*6)+1;
+console.log(num);
+
+
+// //Question 2
+// let car = {
+//     name : "Auddi",
+//     model : 2023,
+//     color : "silver"
+// }
+// console.log(car.name);
+
+// //Question 3
+// let person = {
+//     name : "Aditya",
+//     age : 22,
+//     city : "Nashik"
+// }
+// console.log(person);
+// person.city = "Newyork";
+// console.log(person);
+// person.country = "USA";
+// console.log(person);
