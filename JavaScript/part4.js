4963dafcb8fa98f7b275d8c8f69e9cad1a35c32f
Shubham Rajput <shubhamrajput0504@gmail.com>
2024-06-20 10:41:46 +0530

    Update part4.js

---
+++ JavaScript/part4.js
@@ -0,0 +1,54 @@
+////Question 1
+// let arr=[1,2,3,4,5,6,2,3];
+// let num = 2;
+// console.log("Before delete all occurence of num: ",arr);
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i] == num){
+//         arr.splice(i, 1);
+//     }
+// }
+// console.log("After delete all occurence of num: ",arr);
+
+
+// //Question 2
+// let number = 1324439499;
+// newNum = number;
+// let count = 0;
+//  while(newNum>0){
+//     count++;
+//     newNum = Math.floor(newNum/10);
+    
+//  }
+//  console.log(count);
+
+// //Question 3
+// let number = 1234526;
+// let newNum = number;
+// let sum = 0;
+// while(newNum>0){
+//     let digit = newNum%10;
+//     sum += digit;
+//     newNum = Math.floor(newNum/10);
+// }
+// console.log(sum);
+
+// //Question 4
+// let num = prompt("Enter Number : ");
+// let fact = 1;
+// for(let i=1;i<=num;i++){
+//     fact *= i;
+// }
+// console.log(fact);
+
+// //Question 5
+// let num = [5,6,3,2,56,87,300,34,0];
+// let com = num[0];
+// for(let i=1;i<num.length;i++){
+//     if(com>num[i]){
+//         continue;
+//     }
+//     else{
+//         com = num[i];
+//     }
+// }
+// console.log(com);
