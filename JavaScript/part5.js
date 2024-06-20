4b3d15e8978de3e5a6180203ba0ae219cd8c3b85
Shubham Rajput <shubhamrajput0504@gmail.com>
2024-06-20 10:41:43 +0530

    Create part5.js

---
+++ JavaScript/part5.js
@@ -0,0 +1,18 @@
+// const student = {
+//     name : "Shubham",
+//     age : 21,
+//     branch : "CSS"
+// }
+//  console.log(student);
+
+// //twitter post
+// let post = {
+//     username : "shubhamrajput",
+//     content : "This is my coding post",
+//     likes : 150,
+//     repost : 44,
+//     tags : ["@shubham","@om"]
+// }
+// console.log(post);
+// console.log(post["username"]);
+// console.log(post.repost);
