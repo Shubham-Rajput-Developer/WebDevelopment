01575dbdc93049abd516b3098527b21480411e0e
Shubham Rajput <shubhamrajput0504@gmail.com>
2024-06-22 16:04:57 +0530

    Create part7.js

---
+++ JavaScript/part7.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5];
+// let print = function(el){
+//     console.log(el);
+// }
+// arr.forEach(print);
+
+// arr.forEach(function(el){
+//     console.log(el);
+// }); 
+// arr.forEach( (ele) => (console.log(ele)));
