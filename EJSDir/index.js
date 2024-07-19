8ed90ef85fc31b7da3183ea5462a34476f1fe2c1
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-07-19 21:49:21 +0530

    adding data.json

--- EJSDir/index.js
+++ EJSDir/index.js
@@ -12,3 +12,15 @@ app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     res.render("home");
 })
+
+app.get("/rollDice",(req,res)=>{
+    let dicevalue = Math.floor(Math.random()*6)+1 ;
+    res.render("rollDice.ejs",{num : dicevalue});
+});
+
+app.get("/ig/:username",(req,res)=>{
+    const user = ["shubs","abhi","chetya","omya"];
+    let {username}=req.params;
+    res.render("instagram.ejs",{username,user});
+});
+
