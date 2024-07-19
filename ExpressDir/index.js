b9af7411d53a6ec9745411cbadb95dd98a21db7f
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-07-19 07:24:31 +0530

    change

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -7,7 +7,27 @@ app.listen(port, ()=>{
     console.log(`App is listenning on port ${port}`);
 });
 
-app.use((req,res)=>{
-    console.log(req);
-    console.log("request received");
+// app.use((req,res)=>{
+//     res.send("<h1>Shubham</h1>");
+//     console.log("request received");
+// });
+
+app.get("/",(req,res)=>{
+    res.send("you got res");
+});
+
+app.get("/apple",(req,res)=>{
+    res.send("you on apple path");
+});
+
+app.get("/orange",(req,res)=>{
+    res.send("you on orange path");
+});
+
+app.get("*",(req,res)=>{
+    res.send(" does not exist");
+});
+
+app.post("/",(req,res)=>{
+    res.send("It is Post root Path");
 });
