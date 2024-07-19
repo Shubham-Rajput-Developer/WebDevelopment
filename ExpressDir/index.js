65817fb438b054bd946134a750436247fead9d2b
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-07-19 13:59:36 +0530

    changes

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -16,18 +16,33 @@ app.get("/",(req,res)=>{
     res.send("you got res");
 });
 
-app.get("/apple",(req,res)=>{
-    res.send("you on apple path");
-});
+// app.get("/apple",(req,res)=>{
+//     res.send("you on apple path");
+// });
 
-app.get("/orange",(req,res)=>{
-    res.send("you on orange path");
-});
+// app.get("/orange",(req,res)=>{
+//     res.send("you on orange path");
+// });
 
-app.get("*",(req,res)=>{
-    res.send(" does not exist");
-});
+// app.get("*",(req,res)=>{
+//     res.send(" does not exist");
+// });
 
-app.post("/",(req,res)=>{
-    res.send("It is Post root Path");
+// app.post("/",(req,res)=>{
+//     res.send("It is Post root Path");
+// });
+
+// app.get("/:username/:id",(req,res)=>{
+//     let {username,id} = req.params;
+//     res.send(`Hello @${username} , your id is : ${id}`);
+// });
+
+app.get("/search",(req,res)=>{
+    // console.log(req.query);
+    // res.send("No result");
+    let {q} = req.query;
+    if(!q){
+        res.send("Nothing Search");
+    }
+    res.send(`search result for Query: ${q}`);
 });
