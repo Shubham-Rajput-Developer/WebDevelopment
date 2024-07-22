23ed97ec40d162a08c5fde5b67c1d331eaad3143
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-07-22 20:03:04 +0530

    new changes

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -14,14 +14,17 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
+        id : "1a",
         username : "Shubham",
         content : "I love codding",
     },
     {
+        id : "2b",
         username : "Akshada",
         content : "I have conduct a AI workshop in college",
     },
     {
+        id : "3c",
         username : "Om",
         content : "I am Trader",
     },
@@ -30,6 +33,22 @@ app.get("/posts",(req,res)=>{
     res.render("index.ejs",{ posts });
 })
 
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=>id === p.id);
+    console.log(post);
+    res.send("Done");
+});
 
 app.listen(port,()=>{
     console.log(`Listeaning to port ${port}`);
