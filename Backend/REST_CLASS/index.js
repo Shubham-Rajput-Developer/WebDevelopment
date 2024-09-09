6b643a6a58ac913f62026b7d3229078df7b17fc5
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-09-09 12:35:00 +0530

    Save

--- Backend/REST_CLASS/index.js
+++ Backend/REST_CLASS/index.js
@@ -1,55 +1,80 @@
-const express = require("express");
-const app = express();
+const express = require("express"); //1
+const app = express(); //2
 
-let port = 8080;
+let port = 8080;//3
 
-const path = require("path");
+const {v4:uuidv4} = require("uuid"); //20
 
-app.use(express.urlencoded({extended:true}));
 
-app.set("views engine","ejs");
-app.set("views", path.join(__dirname,"views"));
+const path = require("path"); //5
 
-app.use(express.static(path.join(__dirname,"public")));
+const methodoverride = require("method-override"); //25
+app.use(methodoverride("_method")); //26 
 
-let posts = [
+app.use(express.urlencoded({extended:true})); //9
+
+app.set("views engine","ejs"); //6
+app.set("views", path.join(__dirname,"views")); //7
+
+app.use(express.static(path.join(__dirname,"public"))); //8
+
+let posts = [  //10   //17 create new id to array
     {
-        id : "1a",
+        id : uuidv4(),
         username : "Shubham",
         content : "I love codding",
     },
     {
-        id : "2b",
+        id : uuidv4(),
         username : "Akshada",
         content : "I have conduct a AI workshop in college",
     },
     {
-        id : "3c",
+        id : uuidv4(),
         username : "Om",
         content : "I am Trader",
     },
 ];
-app.get("/posts",(req,res)=>{
+app.get("/posts",(req,res)=>{ //11    //12 create index.ejs file in views folder  //13 create style.css file in public folder
     res.render("index.ejs",{ posts });
 })
 
-app.get("/posts/new",(req,res)=>{
+app.get("/posts/new",(req,res)=>{ //14   //15 create new.ejs file in views folders
     res.render("new.ejs");
 });
 
-app.post("/posts",(req,res)=>{
+app.post("/posts",(req,res)=>{  //16  this work after submiting form data then that display on /posts page 
     let {username,content} = req.body;
-    posts.push({username,content});
+    let id = uuidv4();          //21
+    posts.push({id,username,content});
     res.redirect("/posts");
 });
 
-app.get("/posts/:id",(req,res)=>{
+app.get("/posts/:id",(req,res)=>{   //18 create new path for specific id
     let {id} = req.params;
     let post = posts.find((p)=>id === p.id);
-    console.log(post);
-    res.send("Done");
+    res.render("show.ejs",{post});              //19 create show.ejs 
 });
 
-app.listen(port,()=>{
-    console.log(`Listeaning to port ${port}`);
+app.patch("/posts/:id",(req,res)=>{   //22
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,res)=>{   //23   //24 create edit.ejs file 
+    let {id} = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs",{post});
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=> id !== p.id);
+    res.redirect("/posts");
 })
+app.listen(port,()=>{ //4
+    console.log(`Listeaning to port ${port}`);
+});
