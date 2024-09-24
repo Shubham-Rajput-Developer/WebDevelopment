f7565c2096fe91e668304de6e24d521a0fc7c635
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-09-24 21:51:06 +0530

    done

--- Database/MONGO2/index.js
+++ Database/MONGO2/index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs"); 
 app.use(express.static(path.join(__dirname,"public")));
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 main()
 .then(()=>{
     console.log("connection successfull");
@@ -26,11 +28,51 @@ app.get("/",(req,res)=>{
 //Index route
 app.get("/chats", async(req,res)=>{
     let chats = await Chat.find();
-    console.log(chats);
     res.render("index.ejs",{chats});
-
 });
-
+//Create new message
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+});
+//add new message
+app.post("/chats",(req,res)=>{
+    let {from,to,message} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        message: message,
+        created_at: new Date()
+    });
+    newChat.save()
+    .then((res)=>{
+        console.log(res);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+});
+//Edit 
+app.get("/chats/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+//Update
+app.put("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    let {message:newMessage} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id, {message:newMessage},{runValidators:true,new:true});
+    console.log(updatedChat);
+    res.redirect("/chats"); 
+});
+//destroy
+app.delete("/chats/:id", async(req,res)=>{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+});
 
 app.listen(8080,()=>{
     console.log("Sever is listenning at port 8080");
