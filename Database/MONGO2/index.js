52efa072834cdfa570f1455eaa91d09cfa654369
Shubham-Rajput-Developer <shubhamrajput0504@gmail.com>
2024-09-27 15:20:07 +0530

    Done

--- Database/MONGO2/index.js
+++ Database/MONGO2/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressErr = require("./ExpressErr.js");
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs"); 
@@ -18,7 +19,7 @@ main()
     console.log(err);
 });
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+    await mongoose.connect("mongodb://127.0.0.1:27017/Fakewhatsapp");
 }
 
 app.get("/",(req,res)=>{
@@ -26,16 +27,23 @@ app.get("/",(req,res)=>{
 });
 
 //Index route
-app.get("/chats", async(req,res)=>{
-    let chats = await Chat.find();
-    res.render("index.ejs",{chats});
+app.get("/chats", async(req,res,next)=>{
+    try{
+        let chats = await Chat.find();
+        res.render("index.ejs",{chats});
+    }
+    catch(err){
+        next(err);
+    }
+    
 });
 //Create new message
 app.get("/chats/new",(req,res)=>{
     res.render("new.ejs");
 });
 //add new message
-app.post("/chats",(req,res)=>{
+app.post("/chats",async(req,res,next)=>{
+    try{
     let {from,to,message} = req.body;
     let newChat = new Chat({
         from: from,
@@ -43,35 +51,80 @@ app.post("/chats",(req,res)=>{
         message: message,
         created_at: new Date()
     });
-    newChat.save()
-    .then((res)=>{
-        console.log(res);
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-    res.redirect("/chats");
+    await newChat.save();
+    res.redirect("/chats"); 
+    }catch(err){
+        next(err);
+    }
 });
+
+function asyncWrap(fn){
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=> next(err));
+    };
+}
+//new example routes
+app.get("/chats/:id",asyncWrap(async(req,res,next)=>{
+        let {id} = req.params;
+        let chat = await Chat.findById(id);
+        if(!chat){
+            next( new ExpressErr(404,"chat not found"));
+        }
+        res.render("edit.ejs",{chat});
+}));
+
 //Edit 
-app.get("/chats/:id/edit",async(req,res)=>{
+app.get("/chats/:id/edit",async(req,res,next)=>{
+   try{
     let {id} = req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs",{chat});
+   }catch(err){
+    next(err);
+   }
 });
 //Update
-app.put("/chats/:id", async(req,res)=>{
+app.put("/chats/:id", async(req,res,next)=>{
+    try{
     let {id} = req.params;
     let {message:newMessage} = req.body;
     let updatedChat = await Chat.findByIdAndUpdate(id, {message:newMessage},{runValidators:true,new:true});
     console.log(updatedChat);
     res.redirect("/chats"); 
+    }catch(err){
+        next(err);
+    }
 });
 //destroy
-app.delete("/chats/:id", async(req,res)=>{
+app.delete("/chats/:id", async(req,res,next)=>{
+    try{
     let {id} = req.params;
     let deletedChat = await Chat.findByIdAndDelete(id);
     console.log(deletedChat);
     res.redirect("/chats");
+    }catch(err){
+        next(err);
+    }
+});
+
+const ValidationError = (err)=>{
+    console.log("This is validation error... follow ruless");
+    return err;
+}
+
+//print specific error name and handle it using ValidationError function
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    if(err.name === "ValidationError"){
+        ValidationError(err);   
+    }
+    next(err);
+});
+
+//Default erro handling middlewares
+app.use((err,req,res,next)=>{
+    let {status=500,message="some error"} = err;
+    res.status(status).send(message);
 });
 
 app.listen(8080,()=>{
--- MiddleWares/index.js
+++ MiddleWares/index.js
@@ -1,22 +1,58 @@
 const express = require("express");
 const app = express();
+const ExpressErr = require("./ExpressErr");
+// app.use((req,res,next)=>{
+//     console.log("Hi, I am 1st middlewares");
+//     next();
+// });
 
-app.use((req,res,next)=>{
-    console.log("Hi, I am 1st middlewares");
-    next();
+// app.use((req,res,next)=>{
+//     console.log("Hi, I am 2nd middlewares");
+//     next();
+// });
+
+//logger - morgan
+// app.use((req,res,next)=>{
+//     req.time = Date(Date.now()).toString();
+//     console.log(req.method,req.host,req.path,req.time);
+//     next();
+// });
+
+// app.use((req,res)=>{
+//     res.send("page not found");
+// });
+
+app.use("/api",(req,res,next)=>{
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+    throw new ExpressErr(402,("ACCESS DENIED"));
 });
 
-app.use((req,res,next)=>{
-    console.log("Hi, I am 2nd middlewares");
-    next();
+app.get("/api",(req,res)=>{
+        res.send("dataa");
 });
- 
+
 app.get("/",(req,res)=>{
-    res.send("I am Root");
+    res.send("I am Root"); 
 });
 
 app.get("/random",(req,res)=>{
     res.send("I am random page");
+});
+
+app.get("/err",(req,res)=>{
+    abcd = abcd;
+});
+
+app.use((err,req,res,next)=>{
+    let {status=400, message="Error "} = err;
+    res.status(status).send(message);
+});
+
+app.get("/admin",(req,res)=>{
+    throw new ExpressErr(403,"Access Forbiden");
 });
 
 app.listen(8080,()=>{
